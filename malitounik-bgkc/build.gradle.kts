9caf1615316d652810a7414c1f0d431045348457
oleg <oleg.dydyshko@gmail.com>
2025-09-15 19:20:31 +0300

    Афіцыйны пераклад Рыма-Каталіцкага Касцёла.

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "by.carkva_gazeta.malitounik"
         minSdk = 23
         targetSdk = 36
-        versionCode = 442544
-        versionName = "5.99.22"
+        versionCode = 442545
+        versionName = "5.99.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
