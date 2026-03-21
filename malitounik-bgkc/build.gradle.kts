3c5d70ecc463d885eb7850bfd6f77b121a553e19
oleg <oleg.dydyshko@gmail.com>
2026-03-21 09:41:41 +0300

    Выпраўленне памылак.

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "by.carkva_gazeta.malitounik"
         minSdk = 23
         targetSdk = 36
-        versionCode = 442627
-        versionName = "6.0.5.9"
+        versionCode = 442628
+        versionName = "6.0.5.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
