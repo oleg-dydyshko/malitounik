1e77d5fc495a1faddc423ce43a5da997b34e1b7f
oleg <oleg.dydyshko@gmail.com>
2026-08-09 08:49:12 +0300

    Уладарка і зямлі, і неба

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "by.carkva_gazeta.malitounik"
         minSdk = 23
         targetSdk = 37
-        versionCode = 442661
-        versionName = "6.0.7.19"
+        versionCode = 442662
+        versionName = "6.0.7.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
